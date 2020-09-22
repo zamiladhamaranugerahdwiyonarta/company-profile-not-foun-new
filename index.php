798508ce07ff72ae96b89c70171187ad8b1aa1f4
zamiladhamaranugerahdwiyonarta <you@example.com>
2020-09-22 23:31:09 +0700

    tes commit

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Grow your business with WB productions</h1>
+          <h1 data-aos="fade-up"> tes Grow your business with WB productions</h1>
           <h2 data-aos="fade-up" data-aos-delay="400">We are Team of All Talanted</h2>
           <div data-aos="fade-up" data-aos-delay="800">
             <a href="#about" class="btn-get-started scrollto">Contact Us</a>
